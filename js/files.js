643fcec88e8fb653caf5967fe0cf4ab3283ce1d0
EduuBraga <chocolate.blindado123@gmail.com>
2022-06-16 11:34:46 -0300

    Primeiro commit

---
+++ js/files.js
@@ -0,0 +1,42 @@
+window.files = [
+    {
+        imagem: "./imagens/personagens/luffy.jpg",
+        descricao: 'O personagem principal em um grande anime é quase sempre o personagem mais bem desenvolvido, e em "One Piece" não é exceção. Claro, Luffy pode ser estúpido e imprudente. Sim, ele não dá aos problemas sérios o respeito que eles merecem, e muitas vezes seu próprio descuido o coloca em situações complicadas. No entanto, seu coração inspira sua equipe e todos que se aliam a ele e aos Chapéus de Palha.'
+    },
+    {
+        imagem: "./imagens/personagens/zoro.jpg",
+        descricao: "Zoro é o primeiro a se juntar a Luffy e é o contrapeso perfeito, pois fica calmo quando Luffy fica excitado e disciplinado enquanto Luffy é impulsivo. Zoro também é rápido em defender seu capitão, independentemente de quão horrível seja a situação em que Luffy e suas más escolhas o colocaram. Zoro é um verdadeiro samurai em um mundo de loucos caricaturais."
+    },
+    {
+        imagem: "./imagens/personagens/nami.jpg",
+        descricao: ""
+    },
+    {
+        imagem: "./imagens/personagens/usopp.jpg",
+        descricao: ""
+    },
+    {
+        imagem: "./imagens/personagens/sanji.jpg",
+        descricao: ""
+    },
+    {
+        imagem: "./imagens/personagens/chopper.jpg",
+        descricao: ""
+    },
+    {
+        imagem: "./imagens/personagens/robin.jpg",
+        descricao: ""
+    },
+    {
+        imagem: "./imagens/personagens/franky.jpg",
+        descricao: ""
+    },
+    {
+        imagem: "./imagens/personagens/brook.jpg",
+        descricao: ""
+    },
+    {
+        imagem: "./imagens/personagens/jimbei.jpg",
+        descricao: ""
+    },
+]
---
+++ readme.md
@@ -0,0 +1,6 @@
+#006594 pastel
+#3382A6 um pouco clara
+#8BC5E0 clara
+#004361 escura
+transparente 
+rgb(223, 246, 255)
